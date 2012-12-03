1b263bfe09299a644444103e3fd836c60dde7766
sag47 <sam.mxracer@gmail.com>
2012-12-03 09:10:17 -0500

    Added a few checks and compatibility.
    I added a few preflight tests which I felt were necessary for the
    script to run.
    
    Additionally, I added a few variables so that this script can be
    directly compatible with both JBoss and Tomcat.  I feel like this
    compatibility will allow the deploy script to play nice with other
    app servers as well.

--- appserver-scripts/deploy.sh
+++ appserver-scripts/deploy.sh
@@ -12,6 +12,14 @@
 #  automated deployments from a web interface.  Web interface
 #  automated deployments are for test machines only.
 #
+#  Note the variables are flexible enough that you could configure
+#  this for most app servers.  All you need is an init.d script 
+#  for the app server which can start, stop, and status the server.
+#  Here's a sample config for tomcat:
+#    jbossprofile=/opt/tomcat
+#    deploydir="webapps"
+#    initd_script=/etc/init.d/tomcat
+#
 #Dependencies: 
 #  coreutils-5.97-23.el5_4.2
 #  bash-3.2-24.el5
@@ -25,18 +33,32 @@ export PATH="$PATH:/bin"
 
 ########### CONFIGURATION
 #cd to the staging directory where files to be deployed are kept;the working directory must be the staging area
-cd /opt/staging/
+cd /opt/staging
 #jboss profile for app server
-jbossprofile=/opt/jboss/server/default
+jbossprofile=${jbossprofile:-/opt/jboss/server/default}
+# deploy directory (relative to $jbossprofile)
+deploydir="deploy"
+# lib directory (relative to $jbossprofile)
+libdir="lib"
 #backup copies for deployments
 backupdir=/opt/jboss/server/backup
 #war files to deploy to deploy directory
 war_files="something1.war something2.war"
 #jar files to deploy to lib directory
 lib_files="something1.jar something2.jar"
+#path to init.d service script
+initd_script=/etc/init.d/jboss
+#force JBoss restart every time (0=allow hot deploy, 1=force a restart)
+force_restart=0
 ########### END CONFIGURATION
 
 #run through tests and determine what to deploy (or fail if none)
+if [ ! -f "$initd_script" ];then
+  echo "There is no \$initd_script $initd_script."
+  echo "At a minimum the script must be able to: start, stop, and status the app server."
+  echo "Preflight test failed...  Aborting."
+  exit 1
+fi
 isdeploy=0
 islib=0
 for x in $war_files;do
@@ -66,24 +88,24 @@ if [ ! -d "$backupdir" ];then
   echo -n "Creating directory..."
   mkdir -p "$backupdir" && echo "Done." || echo "Failed."
 fi
-if [ ! -d "$backupdir/deploy" ];then
-  mkdir -p "$backupdir/deploy"
+if [ ! -d "$backupdir/$deploydir" ];then
+  mkdir -p "$backupdir/$deploydir"
 fi
-if [ ! -d "$backupdir/lib" ];then
-  mkdir -p "$backupdir/lib"
+if [ ! -d "$backupdir/$libdir" ];then
+  mkdir -p "$backupdir/$libdir"
 fi
 if [ ! -d "$backupdir" ];then
   echo "Something went wrong with creating \$backupdir $backupdir."
   echo "Preflight test failed...  Aborting."
   exit 1
 fi
-if [ ! -d "$backupdir/deploy" ];then
-  echo "Something went wrong with creating \$backupdir/deploy $backupdir/deploy."
+if [ ! -d "$backupdir/$deploydir" ];then
+  echo "Something went wrong with creating \$backupdir/$deploydir $backupdir/$deploydir."
   echo "Preflight test failed...  Aborting."
   exit 1
 fi
-if [ ! -d "$backupdir/lib" ];then
-  echo "Something went wrong with creating \$backupdir/lib $backupdir/lib."
+if [ ! -d "$backupdir/$libdir" ];then
+  echo "Something went wrong with creating \$backupdir/$libdir $backupdir/$libdir."
   echo "Preflight test failed...  Aborting."
   exit 1
 fi
@@ -94,8 +116,8 @@ function deploy_wars() {
     if [ -e "$x" ];then
       chown jboss\: "$x"
       chmod 644 "$x"
-      mv -f "$x" "$jbossprofile/deploy/$x"
-      touch "$jbossprofile/deploy/$x"
+      mv -f "$x" "$jbossprofile/$deploydir/$x"
+      touch "$jbossprofile/$deploydir/$x"
       echo "$x deployed."
     fi
   done
@@ -105,8 +127,8 @@ function deploy_libs() {
     if [ -e "$x" ];then
       chown jboss\: "$x"
       chmod 644 "$x"
-      mv -f "$x" "$jbossprofile/lib/$x"
-      touch "$jbossprofile/lib/$x"
+      mv -f "$x" "$jbossprofile/$libdir/$x"
+      touch "$jbossprofile/$libdir/$x"
       echo "$x deployed."
     fi
   done
@@ -116,17 +138,17 @@ function deploy_libs() {
 echo -n "Creating backups..."
 pushd "$jbossprofile" > /dev/null
 if [ "$isdeploy" = "1" ];then
-  tar -czf "$backupdir/deploy/deploy_$(date +%Y-%m-%d-%s).tar.gz" deploy
+  tar -czf "$backupdir/$deploydir/$deploydir_$(date +%Y-%m-%d-%s).tar.gz" "$deploydir"
 fi
 if [ "$islib" = "1" ];then
-  tar -czf "$backupdir/lib/lib_$(date +%Y-%m-%d-%s).tar.gz" lib
+  tar -czf "$backupdir/$libdir/$libdir_$(date +%Y-%m-%d-%s).tar.gz" "$libdir"
 fi
 popd > /dev/null
 echo "Done."
 
 #check to see if server shutdown is required
-if [ "$islib" = "1" ];then
-  /etc/init.d/jboss stop
+if [ "$islib" = "1" ] || [ ! "$force_restart" = "0" -a ! "$force_restart" = "false" ];then
+  $initd_script stop
 fi
 
 #execute deployments
@@ -134,8 +156,8 @@ deploy_wars
 deploy_libs
 
 #check to see if server startup is required
-if [ "$islib" = "1" ];then
-  /etc/init.d/jboss start
-  sleep 2 && /etc/init.d/jboss status
+if [ "$islib" = "1" ] || [ ! "$force_restart" = "0" -a ! "$force_restart" = "false" ];then
+  $initd_script start
+  sleep 2 && $initd_script status
 fi
 
