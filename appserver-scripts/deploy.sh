40828b7d55fb37371ab0d215b2b4cc2eb366e6c1
sag47 <sam.mxracer@gmail.com>
2013-03-15 13:57:23 -0400

    Shiny new deploy.sh
    - Fixed variable bugs where $var was being read as $var_. Now ${var}_
    - Fixed some quoting bugs.
    - All new debugging output.  A complete status of the deployment for
      each step.
    - Script now properly utilizes stdout, stderr, and stdin.
    - Script returns proper exit codes for bash logic.

--- appserver-scripts/deploy.sh
+++ appserver-scripts/deploy.sh
@@ -29,7 +29,7 @@
 #
 #  Also depends on my custom /etc/init.d/jboss script.
 
-export PATH="$PATH:/bin"
+export PATH="${PATH}:/bin"
 
 ########### USER CONFIGURATION
 #It is recommended that you go through each default value and specify your configuration
@@ -43,22 +43,23 @@ export PATH="$PATH:/bin"
 #backupdir=""
 #initd_script=""
 #force_restart=0
+#debug=false
 ########### END USER CONFIGURATION
 
 ########### DEFAULT CONFIGURATION
 #cd to the staging directory where files to be deployed are kept;the working directory must be the staging area
-cd "${stage:-/opt/staging}"
-#war files to deploy to deploy directory
-war_files="${war_files:-something1.war something2.war}"
-#jar files to deploy to lib directory
-lib_files="${lib_files:-something1.jar something2.jar}"
+stage="${stage:-/opt/staging}"
+#war files to deploy to deploy directory; add space separated list of war files
+war_files="${war_files:-}"
+#jar files to deploy to lib directory; add space separated list of jar files
+lib_files="${lib_files:-}"
 #app server profile for app server
-appsprofile="${appsprofile:-/opt/jboss/server/default}"
+appsprofile=${appsprofile:-/opt/jboss/server/default}
 #app server user
 appsuser="${appsuser:-jboss}"
-# deploy directory (relative to $appsprofile)
+# deploy directory (relative to ${appsprofile})
 deploydir="${deploydir:-deploy}"
-# lib directory (relative to $appsprofile)
+# lib directory (relative to ${appsprofile})
 libdir="${libdir:-lib}"
 #backup copies for deployments
 backupdir="${backupdir:-/opt/jboss/server/backup}"
@@ -66,130 +67,244 @@ backupdir="${backupdir:-/opt/jboss/server/backup}"
 initd_script="${initd_script:-/etc/init.d/jboss}"
 #force JBoss restart every time (0=allow hot deploy, 1=force a restart)
 force_restart=${force_restart:-0}
+#enable debugging output (false=no debugging, true=debugging messages enabled)
+debug="${debug:-false}"
 ########### END DEFAULT CONFIGURATION
+
+#export environment variables
+export stage war_files lib_files appsprofile appsuser deploydir libdir backupdir initd_script force_restart debug
+
+#show environment if debugging enabled
+function if_debug_print_environment() {
+  if [ ! "${debug}" = "false" ];then
+    echo "== ENVIRONMENT VARIABLES =="
+    echo ""
+    env
+    echo ""
+    echo "== EXECUTE DEPLOYMENT =="
+  fi
+}
 
 #run through tests and determine what to deploy (or fail if none)
 function preflight_check() {
-  if [ ! -f "$initd_script" ];then
-    echo "There is no \$initd_script $initd_script."
-    echo "At a minimum the script must be able to: start, stop, and status the app server."
-    echo "Preflight test failed...  Aborting."
-    exit 1
+  if [ ! "${debug}" = "true" ] && [ ! "${debug}" = "false" ];then
+    echo "debug=${debug} is not a valid option for debug!  Must be true or false." > /dev/stderr
+    echo "Preflight test failed...  Aborting." > /dev/stderr
+    return 1
+  fi
+  if "${debug}";then
+    echo "enter function ${FUNCNAME}" > /dev/stderr
+  fi
+  STATUS=0
+  if [ ! -f "${initd_script}" ];then
+    echo "There is no \${initd_script} ${initd_script}." > /dev/stderr
+    echo "At a minimum the script must be able to: start, stop, and status the app server." > /dev/stderr
+    echo "Preflight test failed...  Aborting." > /dev/stderr
+    STATUS=1
   fi
   isdeploy=0
   islib=0
-  if [ ! -z "$war_files" ];then
-    for x in $war_files;do
-      if [ -f "$x" ];then
+  if [ ! -z "${war_files}" ];then
+    for x in ${war_files};do
+      if [ -f "${x}" ];then
         isdeploy=1
         break
       fi
     done
   fi
-  if [ ! -z "$lib_files" ];then
-    for x in $lib_files;do
-      if [ -f "$x" ];then
+  if [ ! -z "${lib_files}" ];then
+    for x in ${lib_files};do
+      if [ -f "${x}" ];then
         islib=1
         break
       fi
     done
   fi
-  if [ "$isdeploy" = "0" -a "$islib" = "0" ];then
-    echo "No deployments happened.  There was nothing to deploy."
-    echo "Preflight test failed...  Aborting."
-    exit 1
+  if [ "${isdeploy}" = "0" -a "${islib}" = "0" ];then
+    echo "No deployments happened.  There was nothing to deploy." > /dev/stderr
+    echo "Preflight test failed...  Aborting." > /dev/stderr
+    STATUS=1
   fi
-  if [ ! -d "$appsprofile" ];then
-    echo "\$appsprofile dir does not exist: $appsprofile"
-    echo "Preflight test failed...  Aborting."
-    exit 1
+  if [ ! -d "${appsprofile}" ];then
+    echo "\${appsprofile} dir does not exist: ${appsprofile}" > /dev/stderr
+    echo "Preflight test failed...  Aborting." > /dev/stderr
+    STATUS=1
   fi
-  if [ ! -d "$backupdir" ];then
-    echo "WARNING: \$backupdir $backupdir does not exist."
-    echo -n "Creating directory..."
-    mkdir -p "$backupdir" && echo "Done." || echo "Failed."
+  if [ ! -d "${backupdir}" ];then
+    echo "WARNING: \${backupdir} ${backupdir} does not exist." > /dev/stderr
+    echo -n "Creating directory..." > /dev/stderr
+    mkdir -p "${backupdir}" && echo "Done." > /dev/stderr || echo "Failed." > /dev/stderr
   fi
-  if [ ! -d "$backupdir/$deploydir" ];then
-    mkdir -p "$backupdir/$deploydir"
+  if [ ! -d "${backupdir}/${deploydir}" ];then
+    mkdir -p "${backupdir}/${deploydir}"
   fi
-  if [ ! -d "$backupdir/$libdir" ];then
-    mkdir -p "$backupdir/$libdir"
+  if [ ! -d "${backupdir}/${libdir}" ];then
+    mkdir -p "${backupdir}/${libdir}"
   fi
-  if [ ! -d "$backupdir" ];then
-    echo "Something went wrong with creating \$backupdir $backupdir."
-    echo "Preflight test failed...  Aborting."
-    exit 1
+  if [ ! -d "${backupdir}" ];then
+    echo "Something went wrong with creating \${backupdir} ${backupdir}." > /dev/stderr
+    echo "Preflight test failed...  Aborting." > /dev/stderr
+    STATUS=1
   fi
-  if [ ! -d "$backupdir/$deploydir" ];then
-    echo "Something went wrong with creating \$backupdir/$deploydir $backupdir/$deploydir."
-    echo "Preflight test failed...  Aborting."
-    exit 1
+  if [ ! -d "${backupdir}/${deploydir}" ];then
+    echo "Something went wrong with creating \${backupdir}/${deploydir} ${backupdir}/${deploydir}." > /dev/stderr
+    echo "Preflight test failed...  Aborting." > /dev/stderr
+    STATUS=1
   fi
-  if [ ! -d "$backupdir/$libdir" ];then
-    echo "Something went wrong with creating \$backupdir/$libdir $backupdir/$libdir."
-    echo "Preflight test failed...  Aborting."
-    exit 1
+  if [ ! -d "${backupdir}/${libdir}" ];then
+    echo "Something went wrong with creating \${backupdir}/${libdir} ${backupdir}/${libdir}." > /dev/stderr
+    echo "Preflight test failed...  Aborting." > /dev/stderr
   fi
+  if "${debug}";then
+    echo "exit function ${FUNCNAME} return STATUS=${STATUS}" > /dev/stderr
+  fi
+  return ${STATUS}
 }
 
 #deployment logic
 function deploy_wars() {
-  for x in $war_files;do
-    if [ -e "$x" ];then
-      chown $appsuser\: "$x"
-      chmod 644 "$x"
-      mv -f "$x" "$appsprofile/$deploydir/$x"
-      touch "$appsprofile/$deploydir/$x"
-      echo "$x deployed."
+  if "${debug}";then
+    echo "enter function ${FUNCNAME}" > /dev/stderr
+  fi
+  STATUS=0
+  for x in ${war_files};do
+    if [ -e "${x}" ];then
+      #Start of deploy command list
+      chown ${appsuser}\: "${x}" && \
+      chmod 644 "${x}" && \
+      mv -f "${x}" "${appsprofile}/${deploydir}/${x}" && \
+      touch "${appsprofile}/${deploydir}/${x}" && \
+      echo "${x} deployed."
+      #End of deploy command list
+      if [ ! "$?" -eq "0" ];then
+        echo "${x} deployment FAILED!" > /dev/stderr
+        STATUS=1
+        break
+      fi
+    elif "${debug}";then
+      echo "${x} does not exist."
     fi
   done
+  if "${debug}";then
+    echo "exit function ${FUNCNAME} return STATUS=${STATUS}" > /dev/stderr
+  fi
+  return ${STATUS}
 }
 function deploy_libs() {
-  for x in $lib_files;do
-    if [ -e "$x" ];then
-      chown "$appsuser"\: "$x"
-      chmod 644 "$x"
-      mv -f "$x" "$appsprofile/$libdir/$x"
-      touch "$appsprofile/$libdir/$x"
-      echo "$x deployed."
+  if "${debug}";then
+    echo "enter function ${FUNCNAME}" > /dev/stderr
+  fi
+  STATUS=0
+  for x in ${lib_files};do
+    if [ -e "${x}" ];then
+      #Start of deploy command list
+      chown "${appsuser}"\: "${x}" && \
+      chmod 644 "${x}" && \
+      mv -f "${x}" "${appsprofile}/${libdir}/${x}" && \
+      touch "${appsprofile}/${libdir}/${x}" && \
+      echo "${x} deployed."
+      #End of deploy command list
+      if [ ! "$?" -eq "0" ];then
+        echo "${x} deployment FAILED!" > /dev/stderr
+        STATUS=1
+        break
+      fi
+    elif "${debug}";then
+      echo "${x} does not exist." > /dev/stderr
     fi
   done
+  if "${debug}";then
+    echo "exit function ${FUNCNAME} return STATUS=${STATUS}" > /dev/stderr
+  fi
+  return ${STATUS}
 }
 
 #run through and backup everything
 function backup_directories() {
-  echo -n "Creating backups..."
-  pushd "$appsprofile" > /dev/null
-  if [ "$isdeploy" = "1" ];then
-    tar -czf "$backupdir/$deploydir/$deploydir_$(date +%Y-%m-%d-%s).tar.gz" "$deploydir"
+  if "${debug}";then
+    echo "enter function ${FUNCNAME}" > /dev/stderr
   fi
-  if [ "$islib" = "1" ];then
-    tar -czf "$backupdir/$libdir/$libdir_$(date +%Y-%m-%d-%s).tar.gz" "$libdir"
+  STATUS=0
+  echo "Creating backups..."
+  TIME="$(date +%Y-%m-%d-%s)"
+  pushd "${appsprofile}" > /dev/null
+  if [ "${isdeploy}" = "1" ];then
+    echo "${backupdir}/${deploydir}/${deploydir}_${TIME}.tar.gz"
+    if ! tar -czf "${backupdir}/${deploydir}/${deploydir}_${TIME}.tar.gz" "${deploydir}";then
+      echo "Backup FAILED!" > /dev/stderr
+      STATUS=1
+    fi
+  fi
+  if [ "${islib}" = "1" ];then
+    if ! tar -czf "${backupdir}/${libdir}/${libdir_}${TIME}.tar.gz" "${libdir}";then
+      echo "Backup FAILED!" > /dev/stderr
+      STATUS=1
+    fi
   fi
   popd > /dev/null
   echo "Done."
+  if "${debug}";then
+    echo "exit function ${FUNCNAME} return STATUS=${STATUS}" > /dev/stderr
+  fi
+  return ${STATUS}
 }
 
 #check to see if server shutdown is required
 function conditional_shutdown() {
-  if [ "$islib" = "1" ] || [ ! "$force_restart" = "0" -a ! "$force_restart" = "false" ];then
-    "$initd_script" stop
+  if "${debug}";then
+    echo "enter function ${FUNCNAME}" > /dev/stderr
   fi
+  STATUS=0
+  if [ "${islib}" = "1" ] || [ ! "${force_restart}" = "0" -a ! "${force_restart}" = "false" ];then
+    if ! "${initd_script}" stop;then
+      echo "Failed shutting down the app server" > /dev/stderr
+      STATUS=1
+    fi
+  fi
+  if "${debug}";then
+    echo "exit function ${FUNCNAME} return STATUS=${STATUS}" > /dev/stderr
+  fi
+  return ${STATUS}
 }
 
 #check to see if server startup is required
 function conditional_startup() {
-  if [ "$islib" = "1" ] || [ ! "$force_restart" = "0" -a ! "$force_restart" = "false" ];then
-    "$initd_script" start
-    sleep 2 && "$initd_script" status
+  if "${debug}";then
+    echo "enter function ${FUNCNAME}" > /dev/stderr
   fi
+  STATUS=0
+  if [ "${islib}" = "1" ] || [ ! "${force_restart}" = "0" -a ! "${force_restart}" = "false" ];then
+    if ! "${initd_script}" start;then
+      if ! sleep 2 && "${initd_script}" status;then
+        echo "App server failed after apparent successful startup." > /dev/stderr
+        STATUS=1
+      fi
+      echo "Failed to start the app server" > /dev/stderr
+      STATUS=1
+    fi
+  fi
+  if "${debug}";then
+    echo "exit function exit function ${FUNCNAME} return STATUS=${STATUS}" > /dev/stderr
+  fi
+  return ${STATUS}
 }
 
-#execute deployments in a safe order
-preflight_check
-backup_directories
-conditional_shutdown
-deploy_wars
-deploy_libs
+#execute deployments in a safe order; each step depends on a previous
+#stderr will be used for error and debug messages
+#stdout will be used for successful status updates
+#the script will exit with a meaningful status code
+if_debug_print_environment > /dev/stderr
+cd "$stage" && \
+preflight_check && \
+backup_directories && \
+conditional_shutdown && \
+deploy_wars && \
+deploy_libs && \
 conditional_startup
+STATUS=$?
+if "${debug}";then
+  echo "exit STATUS=${STATUS}" > /dev/stderr
+fi
+
+exit ${STATUS}
 
