db19019c2eec62f2f74d69f7d0ff0c0e6f6f319c
sag47 <sam.mxracer@gmail.com>
2013-03-16 22:10:06 -0400

    Balancer manager and others
    - The balancer manager is used to control a balancer-manager
      from mod_proxy_balancer.  This is used for staggered
      deployments on a system.
    - env-security-check takes shell files in stdin and processes
      them line by line to check that only non-system environment
      variables are being assigned and no commands are possibly
      being executed.  This is a security check for cluster
      deployments when a cluster machine pulls an environment file
      from a centralized deploy file for what it should do.  Using
      it is recommended in that scenario.

---
+++ appserver-scripts/env-qa-test.sh
@@ -0,0 +1,17 @@
+# environment file only, no executable code;used to test against env-security-check.sh
+
+#quality testing
+#the following tests blank lines
+
+#the following line test blank lines with a mix of tabs and spaces
+		
+var="$(test)"
+something=(ls)
+echo "I'm bad!"
+TEST_TWO="farts"
+PATH="evil"
+USER="mrclean"
+first_arg="$1"
+#valid entry
+trest="three"
+something2="`ls`"
---
+++ appserver-scripts/env-security-check.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#Created by Sam Gleske (sag47@drexel.edu)
+#Fri Mar  8 17:32:54 EST 2013
+#Red Hat Enterprise Linux Server release 6.3 (Santiago)
+#Linux 2.6.32-279.14.1.el6.x86_64
+#this runs a execution security check on the env.sh file.
+#there should only be static environment variables specified in the env.sh file
+#
+#To run a QA test on this file run the following command
+#  ./env-security-check.sh < ./env-qa-test.sh
+
+#exit code, this may get modified
+result=0
+
+while read line;do
+  if echo "${line}" | grep '^\s*#' &> /dev/null;then
+    #check for comments
+    continue
+  elif echo "${line}" | grep '^\s*$' &> /dev/null;then
+    #check for blank line
+    continue
+  elif echo "${line}" | grep '$(' &> /dev/null || echo "${line}" | grep '`' &> /dev/null;then
+    echo 'security failure: $(list) or `list` command substitution execution is not allowed in env.sh' > /dev/stderr
+    result=1
+  elif echo "${line}" | grep '(' &> /dev/null;then
+    echo 'security failure: (list) subshell execution is not allowed in env.sh' > /dev/stderr
+    result=1
+  elif ! echo "${line}" | grep '^\s*[a-zA-Z_0-9]*=' &> /dev/null;then
+    echo 'security failure: command execution detected.' > /dev/stderr
+    result=1
+  elif echo "${line}" | grep '$[0-9]' &> /dev/null;then
+    echo 'security failure: assignment shell script arguments to variables not allowed in env.sh' > /dev/stderr
+    result=1
+  fi
+  #system_variables list obtained using following command
+  #env | grep '^[a-zA-Z_]*=' | cut -d= -f1 | tr '\n' ' ' | sed 's/\(.*\)/\1\n/'
+  system_variables="HOSTNAME SHELL TERM HISTSIZE USER JAVA_OPTS LS_COLORS TERMCAP PATH MAIL STY PWD JAVA_HOME LANG HISTCONTROL HOME SHLVL LOGNAME CVS_RSH WINDOW LESSOPEN G_BROKEN_FILENAMES _ OLDPWD"
+  for var in ${system_variables};do
+    if echo "${line}" | grep "^\s*${var}=" &> /dev/null;then
+      echo "security failure: assignment of system variable ${var} is not allowed in env.sh" > /dev/stderr
+      result=1
+    fi
+  done
+done
+if [ "${result}" -eq "1" ];then
+  echo 'security notice: env.sh is not a shell script, only an environment file.' > /dev/stderr
+else
+  echo "env.sh security check pass"
+fi
+exit ${result}
