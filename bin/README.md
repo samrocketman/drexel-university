36505355c7649d46bfad338b974ae84b81b9577f
sag47 <sam.mxracer@gmail.com>
2013-08-09 14:28:45 -0400

    Adding the almighty gitar.sh
    A simple script for deduplicating and compressing directories using
    git+tar+(bzip2|gzip).

--- bin/README.md
+++ bin/README.md
@@ -79,3 +79,90 @@ Organize the output by deleted file handle (I've found this to be less useful fo
 Organize the output by process ID and show a heirarchy of deleted file handles as children to the PIDs.  This is the most useful command for determining which services to restart.
 
     wasted-ram-updates.py pids
+
+----
+## gitar.sh - A simple deduplication and compression script
+This is an [original idea by Tom Dalling](http://tomdalling.com/blog/random-stuff/using-git-for-hacky-archive-deduplication/).
+
+`gitar.sh` is a simple deduplication and compression script.  It uses git to deduplicate data and then other compression algorithms to compress data.  This short script was made for when you have to compress a large amount of duplicated files.  It also comes with a handly little utility, `gintar.sh`, for decompressing the archive on the receiving end.  See Usage section for more information.
+
+`gitar.sh` assumes you do not have [lrzip](https://github.com/ckolivas/lrzip) readily available.  lrzip can compress better and deduplicate better than this script.  Also, this script has known limitations which are not bound to lrzip such as not being able to compress git repositories.  gitar.sh is meant as a quick hack 'n slash dedupe and compress.  See the benchmarks for when I tested gitar.sh against other compression methods.
+
+### Compression options
+You can set different compression options with the `compression_type` environment variable.
+
+    #  0 - no compression, just deduplication
+    #  1 - deduplication+optimized git compression
+    #  2 - deduplication+optimized git+gzip compression
+    #  3 - deduplication+optimized git+bzip2 compression
+    export compression_type=3
+
+### Known Limitations/Won't Fix
+* Not able to compress git repositories or nested git repositories.
+* If you're using `gitar.sh` on a directory that contains wholly unique data and no duplicates then the result will actually be slightly larger than using `tar` with `bzip2` or `gzip` due to the metadata of `git`.
+* When extracting a `.gitar` archive using `tar` it is possible to destroy a git repository in the current working directory if one previously exists.
+* It is assumed you will be compressing a single directory.  If you need to compress multiple directories or files then place them all inside of a directory to be gitar'd.
+* You must compress a directory located in the current directory or located in a child.  If the path you're compressing is located above the current working directory in a parent directory then this will fail because git can't do that.
+
+### Usage
+Simply compress a directory.
+
+    gitar.sh "somedirectory"
+
+Subshell the compressing of an archive and set the compression level to 2
+
+    (export compression_type=2; gitar.sh "somedirectory")
+
+Decompress a `gitar` archive.
+
+    tar -xf "somefile.gitar"
+    ./gintar.sh
+
+### Benchmarks of gitar.sh hack vs other utilities
+
+#### Environment
+
+* [Tested Repository](https://github.com/tomdalling/opengl-series) with git directories removed.
+* 3rd Gen Intel® Core™ i7-3770 (Quad Core, 3.40GHz, 8MB L2)
+* 8GB RAM, NON-ECC, 1600MHZ DDR3,2DIMM
+* 512GB Samsung 840 Pro Series 2.5" SSD
+
+Some file system stats for my system using `dd`.
+
+    #READ RATE
+    $ dd if=./coursematerial.tar.gz of=/dev/null
+    1386091+1 records in
+    1386091+1 records out
+    709678734 bytes (710 MB) copied, 4.02507 s, 176 MB/s
+    #WRITE RATE
+    $ dd if=/dev/zero of=./test2
+    827170+0 records in
+    827170+0 records out
+    423511040 bytes (424 MB) copied, 18.1395 s, 23.3 MB/s
+
+Now on to the good stuff....
+
+#### Compression ratios
+For the ratios I used max compression for all utilities (`gzip -9`, and `bzip -9`, and `compression_level=3` for `gitar.sh`, and `lrztar -z` respectively).
+
+    Size    Name                  Type            % of original size
+    132M    opengl-series         Uncompressed    100.0%
+    95M     opengl-series.tar     tar             72.0%
+    30M     opengl-series.tgz     tar+gzip        22.7%
+    27M     opengl-series.tbz2    tar+bzip2       20.5%
+    5.9M    opengl-series.gitar   git+tar+bzip2   4.5%
+    4.4M    opengl-series.tar.lrz tar+lrzip       3.3%
+
+#### Compression times
+I used the `time` utility and took an average of 3 runs for each.
+
+    Name                  Type            real value (from time command)
+    opengl-series         Uncompressed    0m0.000s (no command was executed)
+    opengl-series.tar     tar             0m0.707s
+    opengl-series.tgz     tar+gzip        0m7.200s
+    opengl-series.tbz2    tar+bzip2       0m11.521s
+    opengl-series.gitar   git+tar+bzip2   0m3.977s
+    opengl-series.tar.lrz tar+lrzip       0m24.338s
+
+#### Benchmark Conclusion
+If you want the absolute best compression ratio with the best deduplication then `lrzip` is the utility for you.  However, based on my benchmarks `gitar.sh` achieved a similar ratio in less than one sixth of the time.  `gitar.sh` is a little less user friendly and there are some known drawbacks to using it.  However, if none of these known issues are a problem then `gitar.sh` is great for being fast and highly compressed.  When in doubt of any of the known issues or you're looking for the best compression ratios then `lrzip` is the best candidate.
---
+++ bin/gitar.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+#Created by Sam Gleske
+#original concept by Tom Dalling
+#source: http://tomdalling.com/blog/random-stuff/using-git-for-hacky-archive-deduplication/
+#Fri Aug  9 11:04:46 EDT 2013
+#Ubuntu 12.04.2 LTS
+#Linux 3.8.0-27-generic x86_64
+#GNU bash, version 4.2.25(1)-release (x86_64-pc-linux-gnu)
+
+#KNOWN ISSUES/WON'T FIX:
+#  - Not able to compress git repositories or nested git repositories.
+#  - When extracting a gitar archive using tar it is possible to destroy a git repository
+#    in the current working directory.
+#  - It is assumed you will be compressing a single directory.  If you need to compress
+#    multiple directories or files then place them all inside of a directory to be gitar'd.
+#  - You must compress a directory located in the current directory or located in a child.
+#    If the path you're compressing is located above the current working directory in a
+#    parent then this will fail because git can't do that.
+
+#DESCRIPTION:
+#  This short script was made for when you have to compress a large amount of duplicated
+#  files.  This assumes you do not have lrzip readily available.  Learn more about lrzip at
+#  https://github.com/ckolivas/lrzip
+#
+#  lrzip can compress better and deduplicate better than this script.  Also, this script
+#  has known limitations which are not bound to lrzip such as not being able to compress
+#  git repositories.  This is meant as a quick hack 'n slash dedupe and compress.
+
+#USAGE:
+#  Subshell the compressing of an archive and set the compression level to 2
+#    (export compression_type=2; gitar.sh "somedirectory")
+
+######################################################################
+# List of global options
+######################################################################
+
+#compression types ordered from least to greatest
+#  0 - no compression, just deduplication
+#  1 - deduplication+optimized git compression
+#  2 - deduplication+optimized git+gzip compression
+#  3 - deduplication+optimized git+bzip2 compression
+compression_type="${compression_type:-3}"
+
+
+
+######################################################################
+# List of functions
+######################################################################
+
+function err(){
+  echo "${1}" 1>&2
+}
+function write_gintar(){
+  #copies the currently running program to gintar.sh for unarchiving later
+  cp "$0" "./gintar.sh"
+}
+function preflight(){
+  STATUS=0
+  if [ ! -x "$(which basename)" ];then
+    err "basename executable is missing: GNU coreutils package"
+    STATUS=1
+  fi
+  if [ ! -x "$(which git)" ];then
+    err "git executable is missing: git package"
+    STATUS=1
+  fi
+  if [ ! -x "$(which tar)" ];then
+    err "tar executable is missing: tar package"
+    STATUS=1
+  fi
+  if [ ! -x "$(which bzip2)" ];then
+    err "bzip2 executable is missing: bzip2 package"
+    STATUS=1
+  fi
+  #method specific preflight check based on the script name
+  if [ "${BASENAME}" = "gitar.sh" ];then
+    if [ -d ".git" ];then
+      err "The current directory must not be a git repository!"
+      STATUS=1
+    elif [ ! -z "$(find . -type d -name .git | head -n1)" ];then
+      err "Error, a nested git repository was found.  This is not recommended so will abort."
+      err "To find location run: find . -type d -name .git"
+      STATUS=1
+    fi
+    if [ -f "${1}.gitar" ];then
+      err "${1}.gitar already exists!  Aborting..."
+      STATUS=1
+    fi
+  elif [ "${BASENAME}" = "gintar.sh" ];then
+    if [ ! "${0%/*}" = "${PWD}" -a ! "${0%/*}" = "." ];then
+      err "This script must be run from the same working directory!"
+      err "e.g. ./gintar.sh"
+      STATUS=1
+    elif [ ! -d ".git" ];then
+      err "Missing .git directory.  Was this really from gitar?"
+      STATUS=1
+    fi
+  fi
+  return ${STATUS}
+}
+function gitar(){
+  STATUS=0
+  git init
+  if [ ! "$?" = "0" ];then
+    STATUS=1
+  fi
+  git add "${1}"
+  if [ ! "$?" = "0" ];then
+    STATUS=1
+  fi
+  git commit -m "gitar commit"
+  if [ ! "$?" = "0" ];then
+    STATUS=1
+  fi
+  if [ ! "${compression_type}" = "0" ];then
+    git gc --aggressive
+    if [ ! "$?" = "0" ];then
+      STATUS=1
+    fi
+  fi
+  write_gintar
+  if [ ! "$?" = "0" ];then
+    STATUS=1
+  fi
+  if [ "${compression_type}" = "0" -o "${compression_type}" = "1" ];then
+    tar -cf "${1}".gitar .git gintar.sh
+  elif [ "${compression_type}" = "2" ];then
+    #tar -czf "${1}".gitar .git gintar.sh
+    tar -cf - .git gintar.sh | gzip -9 - > "${1}".gitar
+  elif [ "${compression_type}" = "3" ];then
+    #tar -cjf "${1}".gitar .git gintar.sh
+    tar -cf - .git gintar.sh | bzip2 -9 - > "${1}".gitar
+  else
+    err "Invalid compression type specified in gitar.sh"
+    STATUS=1
+  fi
+  if [ ! "$?" = "0" ];then
+    STATUS=1
+  fi
+  rm -rf ./.git ./gintar.sh
+  if [ ! "$?" = "0" ];then
+    STATUS=1
+  fi
+  return ${STATUS}
+}
+function gintar_ls(){
+  git show --pretty="format:" --name-only $(git log | awk '$1 == "commit" { print $2}')
+  exit
+}
+function gintar(){
+  STATUS=0
+  git reset --hard
+  if [ ! "$?" = "0" ];then
+    STATUS=1
+  fi
+  rm -rf ./.git ./gintar.sh
+  if [ ! "$?" = "0" ];then
+    STATUS=1
+  fi
+  return ${STATUS}
+}
+function success(){
+  if [ "${BASENAME}" = "gitar.sh" ];then
+    err ""
+    err "SUCCESS!"
+    err ""
+    err "Your gitar archive is ready.  To decompress run the following commands."
+    err "tar -xf \"${1}.gitar\""
+    err "./gitar.sh"
+    err ""
+  elif [ "${BASENAME}" = "gintar.sh" ];then
+    err "Successfully extracted!"
+  fi
+  exit 0
+}
+######################################################################
+# Main execution logic
+######################################################################
+
+#execute the script based on the basename.
+BASENAME="$(basename ${0})"
+
+#remove possible trailing slash
+INPUT="${1%/}"
+if [ ! -d "${INPUT}" ];then
+  err "ERROR: ${INPUT} must be a directory!"
+  exit 1
+fi
+
+if [ "${BASENAME}" = "gitar.sh" ];then
+  #start deduplication and compression into an archive
+  if [ "$#" == "0" ];then
+    err "You must provide an argument!"
+    exit 1
+  fi
+  preflight "${INPUT}" && gitar "${INPUT}" && success "${INPUT}"
+  err "A problem has occurred when creating the gitar archive."
+  exit 1
+elif [ "${BASENAME}" = "gintar.sh" ];then
+  #do the gintar.sh action to unarchive
+  if [ "${INPUT}" = "ls" ];then
+    preflight && gintar_ls
+  else
+    preflight && gintar && success
+  fi
+  err "Something has gone very wrong during extraction!"
+  err "For more verbosity run..."
+  err "bash -x ./gintar.sh"
+  exit 1
+else
+  err "Unknown method invoked.  This file must be named gitar.sh or gintar.sh"
+  exit 1
+fi
